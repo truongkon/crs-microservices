ddecc582963ed3528482d91cc0900df55530eefc
truongkon <deptraiketoi2005@gmail.com>
2026-08-06 09:04:32 +0700

    Hoàn thành Lab02

---
+++ course-service/src/main/java/vn/edu/crs/course_service/repository/CourseRepository.java
@@ -0,0 +1,7 @@
+package vn.edu.crs.course_service.repository;
+
+import vn.edu.crs.course_service.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    boolean existsByTenMonHocIgnoreCase(String tenMonHoc);
+}
