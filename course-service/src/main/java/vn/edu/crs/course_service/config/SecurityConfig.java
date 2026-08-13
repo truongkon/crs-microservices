ed1cc1efa14f6081ef5804a22c3da7beb007026c
truongkon <deptraiketoi2005@gmail.com>
2026-08-13 10:47:58 +0700

    init: auth-service with JWT login + seed data

---
+++ course-service/src/main/java/vn/edu/crs/course_service/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package vn.edu.crs.course_service.config;
+import vn.edu.crs.course_service.security.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import
+        org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm ->
+                        sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/internal/**").permitAll() // chi goi tu registration-service qua mang noi bo
+                                .requestMatchers(HttpMethod.GET,
+                                        "/courses/**").permitAll()
+                                .requestMatchers(HttpMethod.POST,
+                                        "/courses/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT,
+                                        "/courses/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.DELETE,
+                                        "/courses/**").hasRole("ADMIN")
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthFilter,
+                        UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
